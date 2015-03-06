d789ff601ea30b55b8ca1a1db4a6b1e9443062ab
theokott <theoakott@gmail.com>
2015-03-06 18:31:26 +0000

    Stopped non-number inputs crashing the program!
    Added validation for the user's input and made the interface a little
    cleaner

--- adder.py
+++ adder.py
@@ -1,17 +1,32 @@
 import string
 import os
-total = 0
-inp = " "
+total = 0.0
+inp = ""
+valid = True
 
-while inp.lower() != "q":
+def isValid(s):
+    if s == "":
+        return True
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def dispText():
     print "Total: ", total
     print "Press 'q' to quit"
     print ""
-    inp = raw_input("Please type in a number or press the enter key to increase the total by 1: ")
-    if inp == "":
-        total = total + 1
-    elif inp not in string.digits:
+
+while inp.lower() != "q":
+    dispText()
+    if valid == False:
         print "Please enter a number"
-    else:
+        print
+    inp = raw_input("Please type in a number or press the enter key to increase the total by 1: ")
+    valid = isValid(inp)
+    if inp == "":
+        total += 1
+    elif valid == True:
         total += float(inp)
     os.system('CLS')
